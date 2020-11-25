82387d3b75b3c2dba70dfa299585de0371967629
shenliang <liang.shen@taiger.com>
2020-11-25 19:36:18 +0800

    implement the get() instead of using the computeIfAbsent from ConcurrentHashMap

--- cache/src/main/java/org/example/MemoryCache.java
+++ cache/src/main/java/org/example/MemoryCache.java
@@ -1,27 +1,78 @@
 package org.example;
 
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.function.Function;
 
 @ThreadSafe
 public class MemoryCache<K, V> implements Cache<K, V> {
-    private final ConcurrentHashMap<K, V> cache = new ConcurrentHashMap<K, V>();
+    // in order to have the fast thread safe get() access & average O(log(n))
+    private final ConcurrentSkipListMap<Holder<K>, Holder<V>> cache = new ConcurrentSkipListMap<>(this::compareKey);
 
     private final Function<K, V> missValueProvider;
+    private final Holder<V> NULL_VALUE = new Holder<>(null);
+    private final Holder<K> NULL_KEY = new Holder<>(null);
+
+
+    private static class Holder<N> {
+        public final N value;
+
+        private Holder(@Nullable N value) {
+            this.value = value;
+        }
+    }
 
     public MemoryCache(Function<K, V> missValueProvider) {
         this.missValueProvider = missValueProvider;
     }
 
-    public V get(K key) {
-        // If not ConcurrentHashMap, need use synchronized() for thread safe
-        // ConcurrentHashMap already implement the thread safe and only call
-        // function once for missing value in computeIfAbsent
-        return cache.computeIfAbsent(key, missValueProvider);
+    // borrow the HashMap hash code function
+    private static int hash(Object key) {
+        int h;
+        return (h = key.hashCode()) ^ (h >>> 16);
     }
 
-    public void put(K key, V value) {
-        cache.put(key, value);
+    private int compareKey(Holder<K> k, Holder<K> k1) {
+        int result = 0;
+        K firstValue = k.value;
+        K secondValue = k1.value;
+
+        if (firstValue instanceof Comparable && secondValue instanceof Comparable) {
+            result = ((Comparable) firstValue).compareTo(secondValue);
+        } else if (firstValue != null && secondValue != null) {
+            result = Integer.compare(hash(firstValue), hash(secondValue));
+        } else {
+            if (firstValue != null)
+                result = 1;
+            else if (secondValue != null)
+                result = -1;
+        }
+
+        return result;
+    }
+
+    public @Nullable
+    V get(@Nullable K key) {
+        Holder<K> keyHolder = key == null ? NULL_KEY : new Holder<>(key);
+        Holder<V> valueHolder = cache.get(keyHolder);
+        if (valueHolder == null) {
+            synchronized (cache) {
+                // need double check for the multiple thread writer
+                valueHolder = cache.get(keyHolder);
+                if (valueHolder == null) {
+                    V raw = missValueProvider.apply(key);
+
+                    if (raw == null)
+                        valueHolder = NULL_VALUE;
+                    else
+                        valueHolder = new Holder<>(raw);
+
+                    cache.put(keyHolder, valueHolder);
+                }
+            }
+        }
+
+        return valueHolder.value;
     }
 }
